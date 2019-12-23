bc24ed195818d9eca8832de9556590414182dc1e
Andy Phung <40963541+andy-phung@users.noreply.github.com>
2019-12-22 16:03:41 -0800

    Update ad8232-capture.ino

--- ad8232-capture.ino
+++ ad8232-capture.ino
@@ -1,27 +1,30 @@
 /******************************************************************************
 development environment specifics:
-	IDE: Arduino 1.0.5
-	Hardware Platform: Redboard Artemis
-	AD8232 Heart Monitor Version: 1.0
+  IDE: Arduino 1.0.5
+  Hardware Platform: Redboard Artemis
+  AD8232 Heart Monitor Version: 1.0
 ******************************************************************************/
+int readECG()
+{
+  delay(1); // Wait for a bit to keep serial data from saturating
+  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+    Serial.println('!');
+    return 0;
+  }
+  else {
+    // send the value of analog input 0:
+      Serial.println(analogRead(A0));
+      return analogRead(A0);
+  }
+  
+}
 
 void setup() {
-  // initialize the serial communication:
   Serial.begin(9600);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
-
 }
 
 void loop() {
-  
-  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-    Serial.println('!');
-  }
-  else{
-    // send the value of analog input 0:
-      Serial.println(analogRead(A0));
-  }
-  //Wait for a bit to keep serial data from saturating
-  delay(1);
+
 }
